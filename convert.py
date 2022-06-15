7f82061f19944f665ae01c060e29013b1ddc87c9
Michael Hubbard <michael.hubbard999@gmail.com>
2022-06-15 13:52:05 -0700

    Fixed PEP8 formatting issues

--- convert.py
+++ convert.py
@@ -16,27 +16,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--number", help="dec, 0b111 or 0x10 style number")
 args = parser.parse_args()
 inpt = args.number
-
-if inpt.find('0b') == 0:
-    inpt = inpt[2:]
+if inpt.find('0b') == 0:  # if 0b is the start of the string
+    inpt = inpt[2:]  # strip off the 0b at the front
     all_binary = all(c in '01' for c in inpt)
-    if all_binary:
+    if all_binary:  # only 0 and 1 in the number
         print(' Binary Value: ', inpt)
         print('Decimal Value: ', int(inpt, 2))
         h = int(inpt, 2)
         print('    Hex Value: ', hex(int(h)))
 
-    elif all_binary == False:
-        print('Number can only contain 0 and 1')  
+    elif all_binary is False:
+        print('Number can only contain 0 and 1')
 elif inpt.find('0x') == 0:
     '''https://www.pythonpool.com/python-hexadecimal-to-decimal/'''
     inpt = inpt[2:]
     try:
-#        if int(inpt, 16):
-            b = int(inpt, 16)
-            print(' Binary Value: ', bin(int(b)))
-            print('Decimal Value: ', int(inpt, 16))
-            print('    Hex Value: ', inpt)
+        b = int(inpt, 16)
+        print(' Binary Value: ', bin(int(b)))
+        print('Decimal Value: ', int(inpt, 16))
+        print('    Hex Value: ', inpt)
     except ValueError:
         print('Number can only contain a-f')
 else:
